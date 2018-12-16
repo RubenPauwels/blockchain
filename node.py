8207dc4158419d9a622fe322b75aa25af0f129ea
RubenPauwels <ruben.alan.j.pauwels@vub.be>
2018-12-16 11:53:02 +0100

    node update

--- node.py
+++ node.py
@@ -1,4 +1,5 @@
 import lib
+import datetime
 class block():
     #this is a block
     def __init__(self,index,amount,timestamp,receiver,sender,PrevHash):
@@ -8,9 +9,21 @@ class block():
         self.receiver=receiver
         self.sender=sender
         self.PrevHash=PrevHash
-        self.Hash="sqd"
+
+        i=str(index)+str(amount)
+
+        self.Hash=lib.hash("sqd")
+
+b="ruben"
+c="kkkkk"
+d=5
+ff= b+c+str(d)
+print ff
+ts=4
+ts=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 
 
 a=block(2,4,23,"kakak","sender","zeze")
 print lib.hash("sqd")
+print a.Hash
