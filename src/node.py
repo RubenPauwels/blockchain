ae7b3b0367c81141373db6725f14a5a31053c395
RubenPauwels <ruben.alan.j.pauwels@vub.be>
2018-12-16 15:08:36 +0100

    wejo ikhaat dit

--- src/node.py
+++ src/node.py
@@ -1,56 +1,77 @@
-import lib
+
+from src.lib import *
 import datetime
+
 import array as arr
-#----------------------------------------------------------------------------------
-b=blockchain()
+
+
+# ----------------------------------------------------------------------------------
+
 class block():
-    #this is a block
-    def __init__(self,index,amount,timestamp_nostri,receiver,sender,PrevHash):
+    # this is a block
+    def __init__(self ,index ,amount ,timestamp_nostri ,receiver ,sender ,PrevHash):
 
-        self.index=index #height of the block
-        self.amount=amount #amount of transaction
-        self.timestamp=timestamp_nostri #time
-        self.receiver=receiver
-        self.sender=sender
-        self.PrevHash=PrevHash #hash of the previous block
-        #we putt everything together
-        i=str(index)+str(amount)+str(timestamp_nostri)+receiver+sender+PrevHash
-        self.Hash=lib.hash(i)
-    def get_LastHash(self,):
-        return Hash
-#----------------------------------------------------------------------------------
+        self.inde x =index  # height of the block
+        self.amoun t =amount  # amount of transaction
+        self.timestam p =timestamp_nostri  # time
+        self.receive r =receiver
+        self.sende r =sender
+        self.PrevHas h =PrevHash  # hash of the previous block
+        # we putt everything together i=str(inde x)+str(amo u nt)+str(tim e stamp_nostri)+receive r +sender+ P revHas h
+        self.Hash=hash(i)
 
-test=block(2,4,datetime.datetime.now(),"kakak","sender","zeze")
-print test.Hash
-print test.timestamp
+    def get_Hash(self):
+        return self.Hash
+    def \
 
-def controle (b,block):
-    #voor het toevoegen, controleren of de hash's goed zijn
-    #step 4 verification
-    if b.get_Hash_lastblock = block.get_Hash
-        integrity = 1;
-    else
-        integrity = 0;
-    #if integrity = 1 -> continue blockchain!!
-    return integrity
+    get_PrevHash(self):
+        return self.PrevHash
 
-#----------------------
+
+# -
+
+
+--------------------------------------------------------------------------------
+
+
+
+def
+
+
+controle (b, block):
+    # voor het toevoegen, controleren of de hash's goed zijn
+    # step 4 verification
+    if b.get_Hash_lastblock() == block.get_PrevHash():
+        integrity = 1
+    else:
+        integrity = 0
+
+
+# ----------------------
 
 
 class blockchain():
     def __init__(self):
-        self.Blockchain_arr=[]
+        self.Blockchain_arr = []
+
     def __add__(self, block):
         self.Blockchain_arr.append(block)
 
-    def get_Hash_lastblock(self):
-        self.Blockchain_arr
-        aa="s"
-        return aa
+    def get_lastblock(self):
+        return "sqd"
 
 
+b = blockchain()
 
+test = block(2, 4, datetime.datetime.now(), "kakak", "sender", "zeze")
+print(test.Hash)
 
 b.__add__(test)
-b
-b.Blockchain_arr.get(1)
+b.__add__(test)
+test = block(2, 4, datetime.datetime.now(), "kaskak", "sender", "zeze")
+print(test.Hash)
+b.__add__(test)
+
+yyy = b.get_lastblock
+pr
+int(yyy)
