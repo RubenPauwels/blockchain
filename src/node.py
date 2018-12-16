3e11ed3978a6a97d9ae460643e1dbc9974cf8a24
Zonnestraal <ulrik_van_hecke@hotmail.com>
2018-12-16 16:22:58 +0100

    readHostFile - a class with functions to read a host file

--- src/node.py
+++ src/node.py
@@ -3,7 +3,46 @@ from src.lib import *
 import datetime
 
 import array as arr
+import os.path
 
+class readHostFile():
+    def readIp_node(nodeNumber):
+        filename = "../config/host_node"+str(nodeNumber)
+        if not os.path.isfile(filename):
+            print("File does not exist")
+        else:
+            with open(filename) as f:
+                content = f.readlines()
+
+                ip_address = content[1].split('= ')
+                ip_address = ip_address[1]
+
+        return ip_address
+    def readIp_reg(nodeNumber):
+        filename = "../config/host_node"+str(nodeNumber)
+        if not os.path.isfile(filename):
+            print("File does not exist")
+        else:
+            with open(filename) as f:
+                content = f.readlines()
+
+                ip_address = content[5].split('= ')
+                ip_address = ip_address[1]
+
+        return ip_address
+    def readIp_neighbors(nodeNumber):
+        filename = "../config/host_node"+str(nodeNumber)
+        if not os.path.isfile(filename):
+            print("File does not exist")
+        else:
+            with open(filename) as f:
+                content = f.readlines()
+                ip_address_neighbors = content[9 :]
+                # for i in ip_address_neighbors:
+                #     print(i)
+
+        return ip_address_neighbors
+    # print(readIp_neighbors(2)[0])
 
 # ----------------------------------------------------------------------------------
 
