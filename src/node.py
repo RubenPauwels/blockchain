e950a8e472bc27e9a1eee920a3cef4cd3813ed88
Cédric <cedric.baijot@vub.be>
2018-12-16 20:16:48 +0100

    server for node

--- src/node.py
+++ src/node.py
@@ -234,3 +234,47 @@ def askIndex_threat(i):
         # send block to server
         test = "sd"
     soc.close();
+
+def Connection_as_server(conn, ip):
+    input_text = read_connection(conn)
+
+    send_connection(conn,"send back same from server:"+input_text)# send it to client
+    conn.close()  # close connection
+    print('Connection ' + str(ip) + ':' + str(portNumber)+ " ended")
+
+
+def start_server():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this is for easy starting/killing the app
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('Socket created on')
+
+    ipaddress = readIp_node(NUMBER_NODE)
+    try:
+        soc.bind((ipaddress, portNumber))
+        print('Socket bind complete')
+    except socket.error as msg:
+        import sys
+        print('Bind failed. Error : ' + str(sys.exc_info()))
+        sys.exit()
+
+    #Start listening on socket
+    soc.listen(10)
+    print('Socket now listening on '+str(ipaddress))
+
+    # for handling task in separate jobs we need threading
+    from threading import Thread
+
+    # this will make an infinite loop needed for
+    # not reseting server for every client
+    while True:
+        conn, addr = soc.accept()
+        ip, port = str(addr[0]), str(addr[1])
+        print('Accepting connection from ' + ip + ':' + port)
+        try:
+            Thread(target=Connection_as_server, args=(conn, ip)).start()
+        except:
+            print("Terible error!")
+            import traceback
+            traceback.print_exc()
+    soc.close()
