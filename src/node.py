74f3df8c76ccd53bc6f7fce75f952f06e190194d
RubenPauwels <ruben.alan.j.pauwels@vub.be>
2018-12-16 15:25:07 +0100

    jjj

--- src/node.py
+++ src/node.py
@@ -11,34 +11,24 @@ class block():
     # this is a block
     def __init__(self ,index ,amount ,timestamp_nostri ,receiver ,sender ,PrevHash):
 
-        self.inde x =index  # height of the block
-        self.amoun t =amount  # amount of transaction
-        self.timestam p =timestamp_nostri  # time
-        self.receive r =receiver
-        self.sende r =sender
-        self.PrevHas h =PrevHash  # hash of the previous block
-        # we putt everything together i=str(inde x)+str(amo u nt)+str(tim e stamp_nostri)+receive r +sender+ P revHas h
+        self.index =index  # height of the block
+        self.amount =amount  # amount of transaction
+        self.timestamp =timestamp_nostri  # time
+        self.receiver =receiver
+        self.sender =sender
+        self.PrevHash =PrevHash  # hash of the previous block
+        # we putt everything together
+        i=str(index)+str(amount)+str(timestamp_nostri)+receiver +sender+ PrevHash
         self.Hash=hash(i)
 
-    def get_Hash(self):
-        return self.Hash
-    def \
-
-    get_PrevHash(self):
-        return self.PrevHash
-
-
-# -
-
-
---------------------------------------------------------------------------------
 
 
 
-def
+# ---------------------------------------------------------------------------------
 
 
-controle (b, block):
+
+def controle (b, block):
     # voor het toevoegen, controleren of de hash's goed zijn
     # step 4 verification
     if b.get_Hash_lastblock() == block.get_PrevHash():
@@ -58,7 +48,10 @@ class blockchain():
         self.Blockchain_arr.append(block)
 
     def get_lastblock(self):
-        return "sqd"
+        a=self.Blockchain_arr[self.Blockchain_arr.__len__()-1]
+        return (a)
+
+
 
 
 b = blockchain()
@@ -67,11 +60,12 @@ test = block(2, 4, datetime.datetime.now(), "kakak", "sender", "zeze")
 print(test.Hash)
 
 b.__add__(test)
-b.__add__(test)
-test = block(2, 4, datetime.datetime.now(), "kaskak", "sender", "zeze")
-print(test.Hash)
+test = block(1, 4, datetime.datetime.now(), "kaskak", "sender", "zeze")
 b.__add__(test)
 
-yyy = b.get_lastblock
-pr
-int(yyy)
+
+
+
+print(test.Hash)
+
+print(b.get_lastblock().Hash)
